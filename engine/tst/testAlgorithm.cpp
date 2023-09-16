160ce82ac4379f04a13c6e7434cf57b7ca0cc1a6
Rusiek <szymon.rusiecki@proton.me>
2023-09-16 12:14:31 +0200

    Add more engine tests

--- engine/tst/testAlgorithm.cpp
+++ engine/tst/testAlgorithm.cpp
@@ -6,85 +6,82 @@ class TestAlgorithm : public ::testing::Test
 {
 public:
     static const engine::Baseline instance;
-    static const uint8_t ok = engine::output_status::correct_pos;
-    static const uint8_t change = engine::output_status::wrong_pos;
-    static const uint8_t wrong = engine::output_status::not_in_word;
 };
 
-TEST(TestAlgorithm, TestValidateAns1)
+TEST_F(TestAlgorithm, TestValidateAns1)
 {
     const std::string ans = "abcde";
     const std::string sol = "abcde";
     const std::array<uint8_t, engine::word_size> ans_info = {
-        TestAlgorithm::ok,
-        TestAlgorithm::ok, 
-        TestAlgorithm::ok,
-        TestAlgorithm::ok,
-        TestAlgorithm::ok};
+        engine::output_status::correct_pos,
+        engine::output_status::correct_pos, 
+        engine::output_status::correct_pos,
+        engine::output_status::correct_pos,
+        engine::output_status::correct_pos};
     EXPECT_EQ(ans_info, TestAlgorithm::instance.validate_ans(ans, sol));
 }
 
-TEST(TestAlgorithm, TestValidateAns2)
+TEST_F(TestAlgorithm, TestValidateAns2)
 {
     const std::string ans = "abcde";
     const std::string sol = "abcdz";
     const std::array<uint8_t, engine::word_size> ans_info = {
-        TestAlgorithm::ok,
-        TestAlgorithm::ok, 
-        TestAlgorithm::ok,
-        TestAlgorithm::ok,
-        TestAlgorithm::wrong};
+        engine::output_status::correct_pos,
+        engine::output_status::correct_pos, 
+        engine::output_status::correct_pos,
+        engine::output_status::correct_pos,
+        engine::output_status::not_in_word};
     EXPECT_EQ(ans_info, TestAlgorithm::instance.validate_ans(ans, sol));
 }
 
-TEST(TestAlgorithm, TestValidateAns3)
+TEST_F(TestAlgorithm, TestValidateAns3)
 {
     const std::string ans = "abcde";
     const std::string sol = "abzed";
     const std::array<uint8_t, engine::word_size> ans_info = {
-        TestAlgorithm::ok,
-        TestAlgorithm::ok, 
-        TestAlgorithm::wrong,
-        TestAlgorithm::change,
-        TestAlgorithm::change};
+        engine::output_status::correct_pos,
+        engine::output_status::correct_pos, 
+        engine::output_status::not_in_word,
+        engine::output_status::wrong_pos,
+        engine::output_status::wrong_pos};
     EXPECT_EQ(ans_info, TestAlgorithm::instance.validate_ans(ans, sol));
 }
 
-TEST(TestAlgorithm, TestValidateAns4)
+TEST_F(TestAlgorithm, TestValidateAns4)
 {
     const std::string ans = "abcde";
     const std::string sol = "xyztq";
     const std::array<uint8_t, engine::word_size> ans_info = {
-        TestAlgorithm::wrong,
-        TestAlgorithm::wrong, 
-        TestAlgorithm::wrong,
-        TestAlgorithm::wrong,
-        TestAlgorithm::wrong};
+        engine::output_status::not_in_word,
+        engine::output_status::not_in_word, 
+        engine::output_status::not_in_word,
+        engine::output_status::not_in_word,
+        engine::output_status::not_in_word};
     EXPECT_EQ(ans_info, TestAlgorithm::instance.validate_ans(ans, sol));
 }
 
-TEST(TestAlgorithm, TestValidateAns5)
+TEST_F(TestAlgorithm, TestValidateAns5)
 {
     const std::string ans = "baaab";
     const std::string sol = "aabaa";
     const std::array<uint8_t, engine::word_size> ans_info = {
-        TestAlgorithm::change,
-        TestAlgorithm::ok, 
-        TestAlgorithm::change,
-        TestAlgorithm::ok,
-        TestAlgorithm::wrong};
+        engine::output_status::wrong_pos,
+        engine::output_status::correct_pos, 
+        engine::output_status::wrong_pos,
+        engine::output_status::correct_pos,
+        engine::output_status::not_in_word};
     EXPECT_EQ(ans_info, TestAlgorithm::instance.validate_ans(ans, sol));
 }
 
-TEST (TestAlgorithm, TestValidateAns6)
+TEST_F(TestAlgorithm, TestValidateAns6)
 {
     const std::string ans = "abcde";
     const std::string sol = "bcdea";
     const std::array<uint8_t, engine::word_size> ans_info = {
-        TestAlgorithm::change,
-        TestAlgorithm::change, 
-        TestAlgorithm::change,
-        TestAlgorithm::change,
-        TestAlgorithm::change};
+        engine::output_status::wrong_pos,
+        engine::output_status::wrong_pos, 
+        engine::output_status::wrong_pos,
+        engine::output_status::wrong_pos,
+        engine::output_status::wrong_pos};
     EXPECT_EQ(ans_info, TestAlgorithm::instance.validate_ans(ans, sol));
 }
